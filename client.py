747917501ebe7cdf4c6b22725be913d822c33352
melaniehendricks <43824979+melaniehendricks@users.noreply.github.com>
2024-02-14 11:27:46 -0700

    client and server can communicate; added arg parsing functionality

---
+++ client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import socket
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--manager-port", required=True, type=int, choices=range(5000,5500))
+# IP not sanitized
+parser.add_argument("--manager-ip", required=True, type=str)
+args = parser.parse_args()
+
+UDP_IP = args.manager_ip
+UDP_PORT = args.manager_port
+MESSAGE = b"Hello, World"
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %d" % UDP_PORT)
+print("message: %s" % MESSAGE)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
---
+++ server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import socket
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", required=True, type=int, choices=range(5000,5500))
+args = parser.parse_args()
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = args.port
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("attempting to bind to %s" % UDP_IP)
+print("on port %d" % UDP_PORT)
+sock.bind((UDP_IP, UDP_PORT))
+print("bind successful")
+
+
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print("received message: %s" % data)
